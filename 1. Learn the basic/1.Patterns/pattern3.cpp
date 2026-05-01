a038e290cda721d0635d7f3cf786fa86d47f2e56
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-05-01 21:50:15 +0530

    Pattern 3 Complete

---
+++ 1. Learn the basic/1.Patterns/pattern3.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+void pattern3(int n){
+for(int i = 1 ; i<n+1;i++){
+        for (int j = 1 ; j<i+1; j++){
+            cout<<j;
+    }
+    cout<<endl;
+}
+}
+int main(){
+    int n;
+    cout<<"Enter the Value of n : ";
+    cin>>n;
+    pattern3(n);
+return 0;
+}
