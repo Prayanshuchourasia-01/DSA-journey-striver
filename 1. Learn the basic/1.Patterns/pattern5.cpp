0d79552b8634e6eeddeaa464ee000efb143c5ea9
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-05-02 08:54:32 +0530

    Pattern 5 Completed

---
+++ 1. Learn the basic/1.Patterns/pattern5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void pattern5(int n){
+    for(int i = 0 ; i<n;i++){
+        for(int j = i ;j<n;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter THe Value Of N : ";
+    cin>>n;
+    pattern5(n);
+
+    return 0;
+}
