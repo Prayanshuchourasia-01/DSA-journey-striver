2431ee94873ca0cd2f7540f3ea223e0307fae016
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-05-03 20:47:36 +0530

    Vector Decaleration

--- 1. Learn the basic/2.Learn STL_Java Collection/STL.cpp
+++ 1. Learn the basic/2.Learn STL_Java Collection/STL.cpp
@@ -16,6 +16,35 @@ void explainPair(){
 
 }
 
+
+// Containers From Here 
+
+void explainVector(){
+    vector <int> v;
+    v.push_back(1);
+    v.emplace_back(2);
+    v.push_back(1);
+    v.emplace_back(2);    
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector <pair<int,int>> vp;
+    vp.push_back({1,2});
+    vp.emplace_back(3,4);
+
+    vector <int> ve(5,_);
+    vector <int> v1(5);
+
+    vector <int> v2(5,20);
+    vector <int> v3(v1);
+
+    // // How to access vector using iterator 
+    // vector<int>::iterator it = v.begin();
+
+
+
+}
+
 int main(){
 
 // Pair
