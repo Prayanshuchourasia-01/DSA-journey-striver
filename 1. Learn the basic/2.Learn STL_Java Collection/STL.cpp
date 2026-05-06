dc8c7d31947f10004d30d8c81bce1f7df2d08aa2
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-05-06 22:33:11 +0530

    Vector 50% complete

--- 1. Learn the basic/2.Learn STL_Java Collection/STL.cpp
+++ 1. Learn the basic/2.Learn STL_Java Collection/STL.cpp
@@ -38,8 +38,37 @@ void explainVector(){
     vector <int> v2(5,20);
     vector <int> v3(v1);
 
-    // // How to access vector using iterator 
-    // vector<int>::iterator it = v.begin();
+    // How to access vector using iterator 
+    vector<int>::iterator it = v.begin();
+    it++;
+    cout<<*(it)<<" ";
+    it = it+2;
+    cout<<*(it)<<" ";
+
+
+    vector<int>::iterator it = v.end();
+    vector<int>::iterator it = v.rend();
+    vector<int>::iterator it = v.rbegin();
+
+    cout<< v[0] << " " <<v.at[0];
+    cout<< v.back() << " ";
+
+    for(vector <int>::iterator it= v.begin();it!=v.end();it++){
+        cout<<*(it) <<" ";
+    }
+
+  for(auto it= v.begin();it!=v.end();it++){
+        cout<<*(it) <<" ";
+    }
+
+    for(auto it : v ){
+        cout<<it<<" ";
+    }
+
+    v.erase(v.begin()+1);
+    v.erase(v.begin()+2,v.begin()+4);
+
+ 
 
 
 
