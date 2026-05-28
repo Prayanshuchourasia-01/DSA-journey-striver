7f58375aa77463305134e1cbc8a7884e197a6990
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-05-28 10:03:56 +0530

    Optimal way

---
+++ 1. Learn the basic/Learn Basic Hashing/optimalWayOfFindingFrequency.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// by using map we can also compute the value more then 10 ke power 9;
+
+int main(){
+
+    int n ;
+    cout<<"Enter The N : "<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i =0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+
+
+    // Precompute 
+   map<int,int> mpp;
+   for(int i = 0 ; i<n ; i++){
+    mpp[arr[i]]++; 
+   }
+
+
+    int q;
+    cout<<"Enter Q  :"<<endl;
+    cin>>q;
+    while(q--){
+        int number ;
+        cout<<"Enter the Number to find "<<endl;
+        cin>>number;
+            //fetch 
+        cout<<mpp[number]<<endl;
+
+
+    }
+
+
+    return 0;
+}
