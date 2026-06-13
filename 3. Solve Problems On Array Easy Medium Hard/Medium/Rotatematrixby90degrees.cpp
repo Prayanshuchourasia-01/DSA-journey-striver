17ef8c139d4881df39c93fd863d9cb2be4e6bd4d
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-06-13 09:56:14 +0530

    Q13 COmplete

---
+++ 3. Solve Problems On Array Easy Medium Hard/Medium/Rotatematrixby90degrees.cpp
@@ -0,0 +1,18 @@
+
+// clockwise direction
+
+#include <bits/stdc++.h> 
+using namespace std;
+vector<vector<int>> rotateMatrix(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for(int i = 0 ; i<n-1;i++){
+        for(int j = i+1 ; j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    for(int i = 0 ; i<n;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+    return matrix;
+}
