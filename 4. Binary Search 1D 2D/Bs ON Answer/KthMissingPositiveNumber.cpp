5bb8212038e8f89f5453dbc488c8b2e05b6b8108
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-07-08 09:04:10 +0530

    Q7 Complete

---
+++ 4. Binary Search 1D 2D/Bs ON Answer/KthMissingPositiveNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int missingK(vector < int > vec, int n, int k) {
+    int low = 0 ; int high = n-1;
+    while(low<=high){
+        int mid = low+(high - low )/2;
+        int missing = vec[mid]-(mid+1);
+        if(missing<k){
+            low = mid +1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return low+k;
+}
