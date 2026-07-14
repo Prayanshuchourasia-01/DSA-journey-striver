c2a140c5571ef52b27e87b3e05b65aff51d1a571
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-07-14 09:56:45 +0530

    Q9 Complete

---
+++ 4. Binary Search 1D 2D/Bs ON Answer/BookAllocationProblem.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxi(vector<int>& arr){
+    int m = INT_MIN;
+    for(int i : arr){
+        m = max(m,i);
+    }
+    return m;
+}
+
+
+int sum(vector<int>& arr){
+    int s = 0;
+    for(int i : arr){
+        s+=i;
+    }
+    return s;
+}
+
+bool possible(vector<int>& arr, int mid, int m){
+    int pages = 0;
+    int stdCnt = 1;
+    for(int i : arr){
+        if(i>mid) return false;
+        if(pages+i>mid){
+            stdCnt++;
+            pages=i;
+        }
+        else{
+            pages+=i;
+        }
+    }
+    if(stdCnt>m){
+        return false;
+    }
+    return true;
+}
+
+
+int findPages(vector<int>& arr, int n, int m) {
+
+    if(m > n)
+        return -1;
+
+    int low = maxi(arr);
+    int high = sum(arr);
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+
+        if(possible(arr, mid, m)){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
